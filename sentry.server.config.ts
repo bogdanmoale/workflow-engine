3086d84ec96b5d76529dc04ad382c98c7d478c2a
bogdanmoale <moale.bogdan.ionica@gmail.com>
2026-04-24 19:23:18 +0300

    implement error traking

---
+++ sentry.server.config.ts
@@ -0,0 +1,14 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://31e3bf76452e94c29ecff312b873e5dc@o4507645194207232.ingest.de.sentry.io/4507645197418576",
+  // Tracing must be enabled for agent monitoring to work
+  tracesSampleRate: 1.0,
+  // Add data like inputs and responses to/from LLMs and tools;
+  // see https://docs.sentry.io/platforms/javascript/data-management/data-collected/ for more info
+  sendDefaultPii: true,
+});
