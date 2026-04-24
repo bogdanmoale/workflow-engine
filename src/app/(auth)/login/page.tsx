39d36458c267653803586d3e0ed8bdcc056f8124
bogdanmoale <moale.bogdan.ionica@gmail.com>
2026-04-24 13:02:23 +0300

    implement better auth

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,7 @@
+import { LoginForm } from "@/features/auth/components/login-form";
+
+const Page = () => {
+  return <LoginForm></LoginForm>;
+};
+
+export default Page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,3 @@
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { getQueryClient, trpc } from "../trpc/server";
-import { ClientGreeting } from "./client-greeting";
-export default async function Home() {
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <ClientGreeting />
-    </HydrationBoundary>
-  );
+export default function Home() {
+  return <div>protected page</div>;
 }
