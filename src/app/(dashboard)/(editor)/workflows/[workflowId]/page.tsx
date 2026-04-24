dc8eb2749e84518a107fb3f26b5b3a5bb1a9c532
bogdanmoale <moale.bogdan.ionica@gmail.com>
2026-04-24 20:18:50 +0300

    impelemt sidebar menu

---
+++ src/app/(dashboard)/(editor)/workflows/[workflowId]/page.tsx
@@ -0,0 +1,16 @@
+import { requireAuth } from "@/lib/auth-utils";
+
+interface PageProps {
+  params: Promise<{
+    workflowId: string;
+  }>;
+}
+
+const WorkflowPage = async ({ params }: PageProps) => {
+  await requireAuth();
+
+  const { workflowId } = await params;
+  return <p>Workflow id : {workflowId}</p>;
+};
+
+export default WorkflowPage;
