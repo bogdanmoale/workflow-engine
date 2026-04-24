071331a290a84bd8e663548d4c5bd0f50028a405
bogdanmoale <moale.bogdan.ionica@gmail.com>
2026-04-24 15:59:35 +0300

    add AI SDK integrations (#6)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { useTRPC } from "@/trpc/client";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { tr } from "date-fns/locale";
 import { toast } from "sonner";
 
 // import { requireAuth } from "@/lib/auth-utils";
@@ -13,7 +14,7 @@ export default function Home() {
   const queryClient = useQueryClient();
   const { data } = useQuery(trpc.getWorkflows.queryOptions());
 
-  const create = useMutation(
+  const createWorkflow = useMutation(
     trpc.createWorkflow.mutationOptions({
       onSuccess: () => {
         toast.success("Workflow creation queued!");
@@ -21,13 +22,23 @@ export default function Home() {
     }),
   );
 
+  const testAi = useMutation(trpc.testAi.mutationOptions());
+
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
       {JSON.stringify(data, null, 2)}
       <Button
-        disabled={create.isPending}
+        disabled={testAi.isPending}
         onClick={() => {
-          create.mutate();
+          testAi.mutate();
+        }}
+      >
+        Test AI
+      </Button>
+      <Button
+        disabled={createWorkflow.isPending}
+        onClick={() => {
+          createWorkflow.mutate();
         }}
       >
         Create Workflow
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,19 +1,59 @@
 import prisma from "@/lib/db";
 import { inngest } from "./client";
+import { createGoogleGenerativeAI } from "@ai-sdk/google";
+import { createOpenAI } from "@ai-sdk/openai";
+import { createAnthropic } from "@ai-sdk/anthropic";
+import { generateText } from "ai";
 
-export const processTask = inngest.createFunction(
-  { id: "process-task", triggers: { event: "app/task.created" } },
+const google = createGoogleGenerativeAI({
+  apiKey: process.env.GOOGLE_GENERATIVE_AI_API_KEY!,
+});
+
+const openAi = createOpenAI({
+  apiKey: process.env.OPENAI_API_KEY!,
+});
+
+const anthropicAi = createAnthropic({
+  apiKey: process.env.ANTHROPIC_API_KEY!,
+});
+
+export const executeAi = inngest.createFunction(
+  { id: "execute-ai", triggers: { event: "execute/ai" } },
   async ({ event, step }) => {
-    await step.sleep("do-something", "5s");
+    const { steps: geminiSteps } = await step.ai.wrap(
+      "gemini-generate-text",
+      generateText,
+      {
+        model: google("gemini-2.5-flash"),
+        system: "You are a helpful assistant",
+        prompt: "What is 2+2?",
+      },
+    );
 
-    await step.sleep("do-something-else", "5s");
+    const { steps: openAiSteps } = await step.ai.wrap(
+      "openai-generate-text",
+      generateText,
+      {
+        model: openAi("gpt-4"),
+        system: "You are a helpful assistant",
+        prompt: "What is 2+2?",
+      },
+    );
 
-    await step.run("create-workflow", async () => {
-      return prisma.workflow.create({
-        data: {
-          name: "Workflow from Inngest",
-        },
-      });
-    });
+    const { steps: anthropicSteps } = await step.ai.wrap(
+      "anthropic-generate-text",
+      generateText,
+      {
+        model: anthropicAi("claude-3-5-sonnet"),
+        system: "You are a helpful assistant",
+        prompt: "What is 2+2?",
+      },
+    );
+
+    return {
+      geminiSteps,
+      openAiSteps,
+      anthropicSteps,
+    };
   },
 );
