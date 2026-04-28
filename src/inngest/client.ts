e4de5140a86a026846b291f82890167a607b2a2e
bogdanmoale <moale.bogdan.ionica@gmail.com>
2026-04-28 13:14:14 +0300

    Add realtime middleware to Inngest initialization

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -1,3 +1,7 @@
+import { realtimeMiddleware } from "@inngest/realtime/middleware";
 import { Inngest } from "inngest";
 
-export const inngest = new Inngest({ id: "workflow-engine" });
+export const inngest = new Inngest({
+  id: "workflow-engine",
+  middleware: [realtimeMiddleware()],
+});
