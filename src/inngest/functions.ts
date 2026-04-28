b70bf025647eae126a331d16949575a848116c59
bogdanmoale <moale.bogdan.ionica@gmail.com>
2026-04-28 18:56:00 +0300

    add gemini node

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,4 +1,4 @@
-import { NonRetriableError } from "inngest";
+import { gemini, NonRetriableError } from "inngest";
 import { inngest } from "./client";
 import prisma from "@/lib/db";
 import { NodeType } from "@/generated/prisma/enums";
@@ -8,6 +8,7 @@ import { httpRequestChannel } from "./channels/http-request";
 import { manualTriggerChannel } from "./channels/manual-trigger";
 import { googleFormTriggerChannel } from "./channels/google-form-trigger";
 import { stripeTriggerChannel } from "./channels/stripe-trigger";
+import { geminiChannel } from "./channels/gemini";
 
 export const executeWorkflow = inngest.createFunction(
   { id: "execute-workflow", retries: 0 }, //TODO : change for production, add exponential backoff
@@ -18,6 +19,7 @@ export const executeWorkflow = inngest.createFunction(
       manualTriggerChannel(),
       googleFormTriggerChannel(),
       stripeTriggerChannel(),
+      geminiChannel(),
     ],
   },
   async ({ event, step, publish }) => {
