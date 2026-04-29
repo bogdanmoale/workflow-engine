1c1304174ed0dc7fb5341f87635af16cb04c37ec
bogdanmoale <moale.bogdan.ionica@gmail.com>
2026-04-29 09:54:20 +0300

    add discord and slack channels to workflow execution

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -11,6 +11,8 @@ import { stripeTriggerChannel } from "./channels/stripe-trigger";
 import { geminiChannel } from "./channels/gemini";
 import { anthropicChannel } from "./channels/anthropic";
 import { openAiChannel } from "./channels/openai";
+import { discordChannel } from "./channels/discord";
+import { slackChannel } from "./channels/slack";
 
 export const executeWorkflow = inngest.createFunction(
   { id: "execute-workflow", retries: 0 }, //TODO : change for production, add exponential backoff
@@ -24,6 +26,8 @@ export const executeWorkflow = inngest.createFunction(
       geminiChannel(),
       anthropicChannel(),
       openAiChannel(),
+      discordChannel(),
+      slackChannel(),
     ],
   },
   async ({ event, step, publish }) => {
@@ -45,6 +49,15 @@ export const executeWorkflow = inngest.createFunction(
       return topologicalSort(workflow.nodes, workflow.connections);
     });
 
+    const userId = await step.run("get-user-id", async () => {
+      const workflow = await prisma.workflow.findUniqueOrThrow({
+        where: { id: workflowId },
+        select: { userId: true },
+      });
+
+      return workflow.userId;
+    });
+
     // Initialize context with any initial data from the trigger
     let context = event.data.initialData || {};
 
@@ -54,6 +67,7 @@ export const executeWorkflow = inngest.createFunction(
       context = await executor({
         data: node.data as Record<string, unknown>,
         nodeId: node.id,
+        userId,
         context,
         step,
         publish,
