1cbf2e075f519f65869ab29d17078a379a1bad7f
bogdanmoale <moale.bogdan.ionica@gmail.com>
2026-05-01 11:28:25 +0300

    dark mode theme (#28)
    * adding dark mode theme
    
    * Update src/components/mode-toggle.tsx

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -1,9 +1,13 @@
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export const AppHeader = () => {
   return (
     <header className="flex h-14 shrink-0 items-center gap-2 border-b px-4 bg-background">
       <SidebarTrigger />
+      <div className="ml-auto">
+        <ModeToggle />
+      </div>
     </header>
   );
 };
